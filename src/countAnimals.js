d9c230d99a2f0b31818b63f2639f5cf7269b3a81
BarbaraRocha <barbaraluizasr@gmail.com>
2022-01-06 17:50:03 -0300

    ex5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,26 @@
 const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
+
+const funcaoVazia = () => species.reduce((acc, especie) => {
+  acc[especie.name] = especie.residents.length;
+  return acc;
+}, {});
+
+const pop = (especie) => species.find((specie) => specie.name === especie.specie).residents.length;
+
+const popularitySex = (especie) => {
+  const { residents } = species.find((specie) => specie.name === especie.specie);
+  return residents.filter((sex) => sex.sex === especie.sex).length;
+};
 
 function countAnimals(animal) {
-  // seu código aqui
+  if (!animal) {
+    return funcaoVazia();
+  }
+  if (Object.keys(animal).length === 1) {
+    return pop(animal);
+  }
+  return popularitySex(animal);
 }
-
+console.log(countAnimals({ specie: 'giraffes' }));
 module.exports = countAnimals;
