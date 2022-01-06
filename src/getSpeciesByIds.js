e155b2b7334c9d3c33f1e69cd1647ca3c8b16612
BarbaraRocha <barbaraluizasr@gmail.com>
2022-01-06 14:43:48 -0300

    ex1s

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,9 @@
 const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+function getSpeciesByIds(...ids) {
+  if (ids.length === 0) return [];
+  return ids.map((id) => species.find((specieId) => specieId.id === id));
 }
-
+console.log(getSpeciesByIds());
 module.exports = getSpeciesByIds;
