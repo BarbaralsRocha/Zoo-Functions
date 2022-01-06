e16689117e3aa17aab7c79e5bf676f0c36f1d53a
BarbaraRocha <barbaraluizasr@gmail.com>
2022-01-06 15:18:18 -0300

    ex2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -3,7 +3,7 @@ const data = require('../data/zoo_data');
 const { species } = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  const getSpecie = species.find((specie) => specie.name === animal).residents
+  const getSpecie = species.find((specie) => specie.name === animal).residents;
   return getSpecie.every((ages) => ages.age > age);
 }
 console.log(getAnimalsOlderThan('otters', 7));
