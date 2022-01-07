628c5569f635cd97dec1e27a471a40ba25e0b45f
BarbaraRocha <barbaraluizasr@gmail.com>
2022-01-07 11:43:02 -0300

    ex6

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,31 @@
 const data = require('../data/zoo_data');
+const { prices } = require('../data/zoo_data');
+
+const entrants = [
+  { name: 'Lara Carvalho', age: 5 },
+  { name: 'Frederico Moreira', age: 5 },
+  { name: 'Pedro Henrique Carvalho', age: 5 },
+  { name: 'Maria Costa', age: 18 },
+  { name: 'Núbia Souza', age: 18 },
+  { name: 'Carlos Nogueira', age: 50 },
+];
 
 function countEntrants(entrants) {
-  // seu código aqui
+  if (typeof entrants === 'undefined' || Object.keys(entrants).length === 0) return 0;
+  const child = entrants.filter((children) => children.age < 18).length;
+  const adult = entrants.filter((adults) => adults.age >= 18 && adults.age < 50).length;
+  const senior = entrants.filter((seniores) => seniores.age >= 50).length;
+  return { child, adult, senior };
 }
+console.log(countEntrants(entrants));
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (typeof entrants === 'undefined' || Object.keys(entrants).length === 0) return 0;
+  const calculate = countEntrants(entrants);
+  const childValue = calculate.child * prices.child;
+  const adultValue = calculate.adult * prices.adult;
+  const seniorValue = calculate.senior * prices.senior;
+  return childValue + adultValue + seniorValue;
 }
-
+console.log(calculateEntry(entrants));
 module.exports = { calculateEntry, countEntrants };
--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,23 +1,47 @@
 const data = require('../data/zoo_data');
 const { species } = require('../data/zoo_data');
 
+const withoutArguments = () => {
+  const funcaoReducer = species.reduce((acumulador, regioes) => {
+    const acc = { ...acumulador };
+    if (!acumulador[regioes.location]) {
+      acc[regioes.location] = [];
+    }
+    acc[regioes.location].push(regioes.name);
+    return acc;
+  }, {});
+  return funcaoReducer;
+};
+
+const nameIncludes = (option) => {
+  const funcaoReducerName = species.reduce((acumulador, regioes) => {
+    const acc = { ...acumulador };
+    if (!acumulador[regioes.location]) {
+      acc[regioes.location] = [];
+    }
+    const names = regioes.residents.map((name) => name.name);
+    const nameSpecies = {};
+    if (option) {
+      const sort = names.sort();
+      nameSpecies[regioes.name] = sort;
+      acc[regioes.location].push(nameSpecies);
+      return acc;
+    }
+    nameSpecies[regioes.name] = names;
+    acc[regioes.location].push(nameSpecies);
+    return acc;
+  }, {});
+  return funcaoReducerName;
+};
+
 function getAnimalMap(options) {
   // Sem parâmetros, retorna animais categorizados por localização;
-  if (!options) {
-    //return species.map((animal) => animal.location);
-    const funcaoReducer = species.reduce((acumulador, regioes) => {
-      if (!acumulador[regioes.location]) {
-        acumulador[regioes.location] = [];
-        return acumulador;
-      }
-      const array = species.filter((regiao) => regiao.location === regioes.location);
-      const mapArray = array.map((names) => names.name);
-      console.log('maparray',mapArray);
-      acumulador[regioes.location].push(mapArray);
-      return acumulador;
-    }, {});
-    return funcaoReducer;
+  if (!options || !options.includeNames) {
+    return withoutArguments();
+  }
+  if (options.includeNames || options.sorted) {
+    return nameIncludes(options);
   }
 }
-console.log(getAnimalMap());
+console.log(getAnimalMap({ includeNames: true, sorted: true }).NE[0]);
 module.exports = getAnimalMap;
